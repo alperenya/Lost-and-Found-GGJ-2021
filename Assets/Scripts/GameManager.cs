92c75299f729953e9f0d7867fab3f84f4ba9ba39
alperenya <62691122+alperenya@users.noreply.github.com>
2021-01-28 23:18:53 +0300

    new prefabs and flower image added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject keyPrefab;
+    public GameObject keyPrefab0;
+    public GameObject keyPrefab1;
+    public GameObject keyPrefab2;
     public GameObject guardPrefab;
     public Transform key0SpawnPosition;
     public Transform key1SpawnPosition;
@@ -17,9 +19,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject key0 = Instantiate(keyPrefab, key0SpawnPosition.position, Quaternion.identity);
-        GameObject key1 = Instantiate(keyPrefab, key1SpawnPosition.position, Quaternion.identity);
-        GameObject key2 = Instantiate(keyPrefab, key2SpawnPosition.position, Quaternion.identity);
+        GameObject key0 = Instantiate(keyPrefab0, key0SpawnPosition.position, Quaternion.identity);
+        GameObject key1 = Instantiate(keyPrefab1, key1SpawnPosition.position, Quaternion.identity);
+        GameObject key2 = Instantiate(keyPrefab2, key2SpawnPosition.position, Quaternion.identity);
         GameObject guard0 = Instantiate(guardPrefab, guard0SpawnPosition.position, Quaternion.identity);
         GameObject guard1 = Instantiate(guardPrefab, guard1SpawnPosition.position, Quaternion.identity);
         GameObject guard2 = Instantiate(guardPrefab, guard2SpawnPosition.position, Quaternion.identity);
