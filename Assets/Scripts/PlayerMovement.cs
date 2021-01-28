859ced5755508276a7806a6fbbea356884a31d00
fturgut9 <77646563+fturgut9@users.noreply.github.com>
2021-01-28 20:48:51 +0300

    kamera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() // used for input
+    {
+        movement.x=Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() // update default in 50 frame per sec for movement
+    {
+        rb.MovePosition(rb.position + movement * speed*Time.fixedDeltaTime);
+    }
+}
