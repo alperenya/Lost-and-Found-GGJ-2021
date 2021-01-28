f26d4d9fb4beab84eb07ff0c29ec5345eb5b3e85
alperenya <62691122+alperenya@users.noreply.github.com>
2021-01-29 01:29:01 +0300

    guards added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,9 @@ public class GameManager : MonoBehaviour
     public GameObject keyPrefab0;
     public GameObject keyPrefab1;
     public GameObject keyPrefab2;
-    public GameObject guardPrefab;
+    public GameObject guardPrefab0;
+    public GameObject guardPrefab1;
+    public GameObject guardPrefab2;
     public Transform key0SpawnPosition;
     public Transform key1SpawnPosition;
     public Transform key2SpawnPosition;
@@ -22,9 +24,9 @@ public class GameManager : MonoBehaviour
         GameObject key0 = Instantiate(keyPrefab0, key0SpawnPosition.position, Quaternion.identity);
         GameObject key1 = Instantiate(keyPrefab1, key1SpawnPosition.position, Quaternion.identity);
         GameObject key2 = Instantiate(keyPrefab2, key2SpawnPosition.position, Quaternion.identity);
-        GameObject guard0 = Instantiate(guardPrefab, guard0SpawnPosition.position, Quaternion.identity);
-        GameObject guard1 = Instantiate(guardPrefab, guard1SpawnPosition.position, Quaternion.identity);
-        GameObject guard2 = Instantiate(guardPrefab, guard2SpawnPosition.position, Quaternion.identity);
+        GameObject guard0 = Instantiate(guardPrefab0, guard0SpawnPosition.position, Quaternion.identity);
+        GameObject guard1 = Instantiate(guardPrefab1, guard1SpawnPosition.position, Quaternion.identity);
+        GameObject guard2 = Instantiate(guardPrefab2, guard2SpawnPosition.position, Quaternion.identity);
 
         key1.GetComponent<KeyController>().number = 1;
         key2.GetComponent<KeyController>().number = 2;
