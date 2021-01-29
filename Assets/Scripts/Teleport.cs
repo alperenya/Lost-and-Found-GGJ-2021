58f7cdf3b7c4d248e063de530e8bd8cbceaeda50
alperenya <62691122+alperenya@users.noreply.github.com>
2021-01-29 15:51:08 +0300

    teleport

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            collider.transform.position = teleportTarget.transform.position;
+        }
+
+    }
+}
