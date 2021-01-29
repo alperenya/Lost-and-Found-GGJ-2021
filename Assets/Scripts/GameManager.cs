e1a984cf95c22261d01a4250ac33e1eacf49381c
alperenya <62691122+alperenya@users.noreply.github.com>
2021-01-29 16:16:00 +0300

    kadın added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,12 +10,14 @@ public class GameManager : MonoBehaviour
     public GameObject guardPrefab0;
     public GameObject guardPrefab1;
     public GameObject guardPrefab2;
+    public GameObject kadinPrefab;
     public Transform key0SpawnPosition;
     public Transform key1SpawnPosition;
     public Transform key2SpawnPosition;
     public Transform guard0SpawnPosition;
     public Transform guard1SpawnPosition;
     public Transform guard2SpawnPosition;
+    public Transform kadınSpawnPosition;
 
 
     // Start is called before the first frame update
@@ -27,6 +29,8 @@ public class GameManager : MonoBehaviour
         GameObject guard0 = Instantiate(guardPrefab0, guard0SpawnPosition.position, Quaternion.identity);
         GameObject guard1 = Instantiate(guardPrefab1, guard1SpawnPosition.position, Quaternion.identity);
         GameObject guard2 = Instantiate(guardPrefab2, guard2SpawnPosition.position, Quaternion.identity);
+        GameObject kadin = Instantiate(kadinPrefab, kadınSpawnPosition.position, Quaternion.identity);
+
 
         key1.GetComponent<KeyController>().number = 1;
         key2.GetComponent<KeyController>().number = 2;
